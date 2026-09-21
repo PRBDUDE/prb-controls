8192fe3c0139b5f178e4c449bc995e892868f47a
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-21 09:05:52 -0400

    add prb layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from '@core/footer';
+import { Header } from '@core/header';
+import { Layout } from '@core/layout';
+import { Content } from '@core/content/content';
 
 @Component({
   selector: 'prb-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Footer, Header, Layout, Content],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('prb-controls');
