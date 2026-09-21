600c509ce02e9266c82673f4d5b6bc08d673f10b
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-21 09:09:51 -0400

    add path-not-found

---
+++ src/app/core/path-not-found/path-not-found.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  imports: [],
+  selector: 'prb-path-not-found',
+  styleUrl: './path-not-found.scss',
+  templateUrl: './path-not-found.html',
+})
+export class PathNotFound {}
