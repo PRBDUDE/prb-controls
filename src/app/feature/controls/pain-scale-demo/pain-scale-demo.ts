0d00e9c94d8ce50f9d0e950d24e93a07138bbb88
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-21 12:32:12 -0400

    add card with description for pain-scale-demo

--- src/app/feature/controls/pain-scale-demo/pain-scale-demo.ts
+++ src/app/feature/controls/pain-scale-demo/pain-scale-demo.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 import { JellyContainer } from '@core/jelly-container';
 import { PainScaleSelector } from '@control/pain-scale-selector';
+import { CardBody } from '@core/card-body';
+import { CardContainer } from '@core/card-container';
+import { CardHeader } from '@core/card-header';
+import { HtmlAttribute } from '@core/html-attribute';
+import { HtmlTag } from '@core/html-tag';
+import { UserTag } from '@core/user-tag';
 
 @Component({
-  imports: [JellyContainer, PainScaleSelector],
+  imports: [
+    JellyContainer,
+    PainScaleSelector,
+    CardBody,
+    CardContainer,
+    CardHeader,
+    HtmlAttribute,
+    HtmlTag,
+    UserTag,
+  ],
   selector: 'prb-pain-scale-demo',
   styleUrl: './pain-scale-demo.scss',
   templateUrl: './pain-scale-demo.html',
