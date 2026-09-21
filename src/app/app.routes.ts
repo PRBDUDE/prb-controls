d364a32988009e6661208221eb028258d6760011
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-21 09:05:27 -0400

    add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: 'home',
+    loadComponent: () => import('./feature/controls/controls').then((m) => m.Controls),
+    title: 'Home',
+  },
+  {
+    path: 'path-not-found',
+    loadComponent: () => import('./core/path-not-found/path-not-found').then((m) => m.PathNotFound),
+    title: 'Path not found',
+  },
+  {
+    path: '**',
+    redirectTo: '/path-not-found',
+    pathMatch: 'full',
+  }
+];
