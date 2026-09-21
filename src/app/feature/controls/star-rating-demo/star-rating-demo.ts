15e8600b158359fc43f0f84a46a3fcf7f3766cfc
PRBDUDE <programmerroadblocks@gmail.com>
2026-09-21 12:21:08 -0400

    add card to display instructions for both star rating demos

--- src/app/feature/controls/star-rating-demo/star-rating-demo.ts
+++ src/app/feature/controls/star-rating-demo/star-rating-demo.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
 import { JellyContainer } from '@core/jelly-container';
 import { StarRating } from '@control/star-rating';
+import { HtmlTag } from '@core/html-tag';
+import { UserTag } from '@core/user-tag';
+import { HtmlAttribute } from '@core/html-attribute';
+import { CardContainer } from '@core/card-container';
+import { CardHeader } from '@core/card-header';
+import { CardBody } from '@core/card-body';
 
 @Component({
-  imports: [JellyContainer, StarRating],
+  imports: [
+    JellyContainer,
+    StarRating,
+    HtmlTag,
+    UserTag,
+    HtmlAttribute,
+    CardContainer,
+    CardHeader,
+    CardBody,
+  ],
   selector: 'prb-star-rating-demo',
   styleUrl: './star-rating-demo.scss',
   templateUrl: './star-rating-demo.html',
